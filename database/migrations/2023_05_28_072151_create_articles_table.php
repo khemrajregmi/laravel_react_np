3f05f3f9a4befaf66366d61b2c3d53fb53b290fe
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-05-31 21:21:30 +0200

    change the nullable condition on article table

--- database/migrations/2023_05_28_072151_create_articles_table.php
+++ database/migrations/2023_05_28_072151_create_articles_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
                 $table->id();
                 $table->string('title');
                 $table->text('content');
-                $table->string('author');
+                $table->string('author')->nullable();
                 $table->string('source');
-                $table->unsignedBigInteger('category_id');
+                $table->unsignedBigInteger('category_id')->nullable();
                 $table->timestamp('published_at')->nullable();
                 $table->timestamps();
                 $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
