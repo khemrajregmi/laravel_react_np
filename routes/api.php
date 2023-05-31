fb48183255b21274b8bac8d59f4a95f87f7db79f
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-05-31 21:22:01 +0200

    add route for news search

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+    Route::get('/news', [NewsController::class, 'search']);
 });
