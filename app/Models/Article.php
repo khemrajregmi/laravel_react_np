be26ce105462070549ed552ca84540b2fa596391
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-06-02 13:59:40 +0200

    minor chages in article model in fillable and so on

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,17 +9,33 @@ class Article extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'articles';
+
+    public mixed $content;
+    public mixed $title;
+    public mixed $author;
+    public mixed $category_id;
+    public mixed $published_at;
+    public mixed $source;
+    protected $primaryKey = 'id';
     protected $fillable = [
+        'id',
         'title',
         'content',
+        'url',
         'author',
         'source',
-        'category',
+        'category_id',
         'published_at'
     ];
 
     // Relationships
-    public function category()
+    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
