e43a621ee55efa668f3f19a7f558c8e4a89e571b
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-06-03 22:31:41 +0200

    add request validation and fronend file added

---
+++ frontend/src/axios.js
@@ -0,0 +1,11 @@
+import Axios from 'axios';
+
+const axios = Axios.create({
+	baseURL: "http://localhost/api",
+	withCredentials: true,
+	headers: {
+		"Content-Type": "application/json",
+		"Accept": "application/json",
+	},
+});
+export default axios;
