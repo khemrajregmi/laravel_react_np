afc56fcbc14d886f80b79b59dddefb5b88413376
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-06-03 23:51:34 +0200

    added filter condition in news backend

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -11,11 +11,35 @@ class NewsController extends Controller
     public function newsFeed(Request $request): \Illuminate\Http\JsonResponse
     {
 //        dd('la ya pugyo');
+//        $perPage = $request->input('per_page', 10);
+//        $page = $request->input('page', 1);
+//
+//        $posts = Article::orderBy('created_at', 'desc')
+//            ->paginate($perPage, ['*'], 'page', $page);
+//
+//        return response()->json([
+//            'data' => $posts->items(),
+//            'totalPages' => $posts->lastPage(),
+//        ]);
         $perPage = $request->input('per_page', 10);
         $page = $request->input('page', 1);
 
-        $posts = Article::orderBy('created_at', 'desc')
-            ->paginate($perPage, ['*'], 'page', $page);
+        $query = Article::with('category')->orderBy('created_at', 'desc');
+
+        // Apply filters
+        if ($request->has('date')) {
+            $query->whereDate('created_at', $request->date);
+        }
+
+        if ($request->has('category')) {
+            $query->where('category_id', $request->category);
+        }
+
+        if ($request->has('source')) {
+            $query->where('source', $request->source);
+        }
+
+        $posts = $query->paginate($perPage, ['*'], 'page', $page);
 
         return response()->json([
             'data' => $posts->items(),
