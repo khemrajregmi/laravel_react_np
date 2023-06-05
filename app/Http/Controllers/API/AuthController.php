86e1d0bc8241b64e9c9994974b78298ffebd21b5
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-06-05 23:48:35 +0200

    added method in news and auth controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\Article;
 use App\Models\User;
 use Validator;
 use App\Http\Controllers\API\BaseController as BaseController;
--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use App\Models\Preference;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    public function newsFeed(Request $request): \Illuminate\Http\JsonResponse
+    public function newsFeed(Request $request): JsonResponse
     {
+//        dd('la ya pugo');
         $perPage = $request->input('per_page', 10);
         $page = $request->input('page', 1);
 
@@ -36,7 +39,7 @@ class NewsController extends Controller
         ]);
     }
 
-    public function index(Request $request): \Illuminate\Http\JsonResponse
+    public function index(Request $request): JsonResponse
     {
         $sources = $request->get('sources', []);
         $categories = $request->get('categories', []);
@@ -59,5 +62,64 @@ class NewsController extends Controller
         $newsFeed = $query->get();
 
         return response()->json(['data' => $newsFeed]);
+    }
+
+    public function userPreferenceNewsFeed(Request $request,$id): JsonResponse
+    {
+//        dd($request);
+//
+//        $perPage = $request->input('per_page', 10);
+//        $page = $request->input('page', 1);
+//
+//        $query = Article::with('category')->where('category_id', $request->category)
+//        ->orderBy('created_at', 'desc');
+
+        // Apply filters
+//        if ($preference->has('date')) {
+//            $query->whereDate('created_at', $request->date);
+//        }
+
+//
+//        if (!empty($preference->source)) {
+//            $query->where('source', $request->source);
+//        }
+//
+//        if (!empty($preference->athor)) {
+//            $query->where('author', $request->author);
+//        }
+////
+//        $posts = $query->paginate($perPage, ['*'], 'page', $page);
+
+//        return response()->json([
+//            'data' => $posts->items(),
+//            'totalPages' => $posts->lastPage(),
+//        ]);
+
+
+        try {
+            $perPage = $request->input('per_page', 10);
+            $page = $request->input('page', 1);
+            // Fetch preference data for the given user
+            $preference = Preference::with('category')->where('user_id', $id)->first();
+//            dd($preference->category_id);
+            $query = Article::with('category')->where('category_id', $preference->category_id)
+                ->orderBy('created_at', 'desc');            //            dd('la ya pugo');
+            if (!empty($preference->source)) {
+                $query->where('source', $request->source);
+            }
+
+            if (!empty($preference->athor)) {
+                $query->where('author', $request->author);
+            }
+    //
+            $posts = $query->paginate($perPage, ['*'], 'page', $page);
+
+            return response()->json([
+                'data' => $posts->items(),
+                'totalPages' => $posts->lastPage(),
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
