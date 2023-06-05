6331713d55a22522c35520ce691d214397c258e0
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-06-05 23:47:16 +0200

    add category relation on prefernce model

--- app/Models/Preference.php
+++ app/Models/Preference.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Preference extends Model
 {
     use HasFactory;
 
+
+    public mixed $source;
+    public mixed $author;
     protected $fillable = [
         'user_id',
         'source',
@@ -24,6 +28,11 @@ class Preference extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 
     /**
